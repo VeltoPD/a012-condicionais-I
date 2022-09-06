1ef3293cf23c681edd52f53d49f65e30ec28556b
VeltoPD <veltopd@gmail.com>
2022-09-06 17:50:14 -0300

    Entrega dos exercícios a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = +prompt("idade?")
+let ensinoMedioCompleto = confirm("Ensino médio completo?")
+let cursaFaculdade = confirm("Cursando faculdade?")
+console.log(typeof(idade)) //O sinal de mais na frente de prompt torna o input number
 
-if(){
+if (idade >= 18) {
+    console.log("A pessoa é maior de idade.")
+} else {
+    console.log("A pessoa é menor de idade.")
+}
 
-}else{
-	
+if (ensinoMedioCompleto == true) {
+    console.log("Ensino médio concluido")
+} else {
+    console.log("Ensino médio incompleto")
+}
+
+if (cursaFaculdade == false) {
+    console.log("Não está cursando faculdade")
+} else {
+    console.log("Está cursando faculdade")
 }
